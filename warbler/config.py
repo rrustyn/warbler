df9693eded1ae471e990b1478c1711103cbbd5f3
Ryan Rustyn <ryanrustyn@gmail.com>
2023-01-20 10:17:09 -0800

    comment out test url

--- warbler/config.py
+++ warbler/config.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.environ['DATABASE_URL']
-DATABASE_URL_TEST = os.environ['DATABASE_URL_TEST']
+#DATABASE_URL_TEST = os.environ['DATABASE_URL_TEST']
 CURR_USER_KEY="curr_user"
